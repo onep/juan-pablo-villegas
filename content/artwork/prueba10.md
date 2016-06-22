c2f14cfbebddbfcb964c1a9a942e1591aed622ca
Victor Gonzalez <aficiomaquinas@users.noreply.github.com>
2016-06-22 13:58:06 -0500

    images and responsive loading
    fixed background-image cover
    images now only load when both:
     - media query is not mobile
     - user agent is not mobile

--- content/artwork/prueba10.md
+++ content/artwork/prueba10.md
@@ -1,6 +1,6 @@
 +++
 date = "2016-06-09T23:08:28-05:00"
-featuredimage = "/img/600-9.jpg"
+featuredimage = "/img/Cat-party.gif"
 title = "prueba10"
 
 +++
--- themes/juan-pablo-villegas/static/ts/app.ts
+++ themes/juan-pablo-villegas/static/ts/app.ts
@@ -1,36 +1,6 @@
 declare var isMobile: any;
 declare var enquire: any;
 
-Modernizr.addTest('ismobile', function() {
- return isMobile.any;
-});
-
-enquire.register("screen and (max-width: 768px)", {
-    match: function () {
-        // console.log('matched mobile');
-        $(".year-image").stick_in_parent({offset_top: 36});
-    },
-    unmatch: function () {
-        // console.log('unmatched mobile');
-        $(".year-image").trigger("sticky_kit:detach");
-    },
-});
-
-enquire.register("screen and (min-width: 769px)", {
-    match: function () {
-        // console.log('matched desktop');
-        $(".pieces").stick_in_parent({offset_top: 36});
-        $(".year-image").stick_in_parent({bottoming: true, offset_top: 72});
-        $(".year-image dt").stick_in_parent({bottoming: false, offset_top: 0});
-    },
-    unmatch: function () {
-        // console.log('unmatched desktop');
-        $(".pieces").trigger("sticky_kit:detach");
-        $(".year-image").trigger("sticky_kit:detach");
-        $(".year-image dt").trigger("sticky_kit:detach");
-    },
-});
-
 function siblingArray(arr:Array<any>, key:string, match:string) {
     let matches;
     matches = arr.filter(function(val, index, array:Array<any>) {
@@ -54,21 +24,54 @@ function toDataUrl(url, callback){
 }
 
 imgArr = [];
-$("dd a").each(function(){
-    let imageUrl = $(this).attr("data-featured-image-url");
-    toDataUrl(imageUrl, function(base64Img){
-        imgArr.push({"url": imageUrl, "base64": base64Img});
-    });
+
+Modernizr.addTest('ismobile', function() {
+ return isMobile.any;
 });
 
-$("dd a").hover(function() {
-    let featuredUrl:string = $(this).attr("data-featured-image-url");
-    let featuredImg = $(this).parents(".year-box").find(".featured-image")
-    let base64 = siblingArray(imgArr, "url", featuredUrl)[0].base64;
-    featuredImg.css("background", "url(" + base64 + ") no-repeat center center" ).css('opacity', '1');
-    
-}, function () {
-    let featuredUrl:string = $(this).attr("data-featured-image-url");
-    let featuredImg = $(this).parents(".year-box").find(".featured-image");
-    featuredImg.css('opacity', '0');
+enquire.register("screen and (max-width: 768px)", {
+    match: function () {
+        $(".year-image").stick_in_parent({offset_top: 36});
+    },
+    unmatch: function () {
+        $(".year-image").trigger("sticky_kit:detach");
+    },
 });
+
+enquire.register("screen and (min-width: 769px)", {
+    deferSetup: true,
+    setup: function () {
+        if(!isMobile.any) {
+            $("dd a").each(function(){
+                let imageUrl = $(this).attr("data-featured-image-url");
+                toDataUrl(imageUrl, function(base64Img){
+                    imgArr.push({"url": imageUrl, "base64": base64Img});
+                });
+            });
+        }
+    },
+    match: function () {
+        $(".pieces").stick_in_parent({offset_top: 36});
+        $(".year-image").stick_in_parent({bottoming: true, offset_top: 72});
+        $(".year-image dt").stick_in_parent({bottoming: false, offset_top: 0});
+        if(!isMobile.any) {
+            $("dd a").hover(function() {
+                let featuredUrl:string = $(this).attr("data-featured-image-url");
+                let featuredImg = $(this).parents(".year-box").find(".featured-image")
+                let base64 = siblingArray(imgArr, "url", featuredUrl)[0].base64;
+                featuredImg.css("background", "url(" + base64 + ") no-repeat center center" ).css('opacity', '1');
+                
+            }, function () {
+                let featuredUrl:string = $(this).attr("data-featured-image-url");
+                let featuredImg = $(this).parents(".year-box").find(".featured-image");
+                featuredImg.css('opacity', '0');
+            });
+        }
+    },
+    unmatch: function () {
+        $(".pieces").trigger("sticky_kit:detach");
+        $(".year-image").trigger("sticky_kit:detach");
+        $(".year-image dt").trigger("sticky_kit:detach");
+    },
+});
+
